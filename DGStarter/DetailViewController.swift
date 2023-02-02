30e564fa363bc975cf54d87c9a54a301afbc21de
Nanolithe <39149226+Nanolithe@users.noreply.github.com>
2023-02-02 13:54:24 -0800

    Add files via upload
    New Update February 2nd

---
+++ DGStarter/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  DGStarter
+//
+//  Created by Kevin Nguyen on 2/2/23.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // Property
+    var dinosaur: Dinosaur?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        if let dinosaur = dinosaur {
+            print(dinosaur.name)
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
